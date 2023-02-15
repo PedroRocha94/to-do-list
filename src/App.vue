db5e17ea890bc8b62ff3a1393a7be6567f55b48e
Pedro Rocha <pedrorocha73658@gmail.com>
2023-02-14 22:08:20 -0300

    Creating Task componente.

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Task />
+</template>
+
+<script>
+import Task from './components/Task.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Task
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'App.scss'
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="task">
+    <fieldset class="fieldset">
+      <legend class="fieldset-title">
+        <div class="fieldset-status">
+          <div class="fieldset-status-color"> </div>
+          <span class="fieldset-status-text">Urgente</span>
+        </div>
+        <span class="fieldset-title-text"> {{ msg }} </span>
+      </legend>
+      <p class="fieldset-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem rem 
+        eligendi eaque doloribus cumque excepturi impedit iusto. Distinctio vero 
+        enim magni deserunt sed quae, quos libero corporis in dolorem impedit.
+      </p>
+    </fieldset>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  data() {
+    return {
+      msg: 'Mandar mensagem para o tio Ben'
+    }
+  }
+}
+</script>
