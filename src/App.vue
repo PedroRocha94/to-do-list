1b167306bfc83edc8adfc5aeb71d16850ffbbffd
Pedro Rocha <pedrorocha73658@gmail.com>
2023-06-24 11:39:08 -0300

    Creating initial project structure.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="app">
-
+    <ToDo />
   </div>
 </template>
 
 <script setup>
-
+  import ToDo from './components/ToDo.vue';
 </script>
 
 <style lang="scss">
---
+++ src/components/ToDo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="to-do">
+    <h1>Lista de Tarefas</h1>
+
+    <div class="input-field">
+      <label>Tarefas: </label>
+      <div>
+        <input type="text">
+        <button>Adicionar</button>
+      </div>
+    </div>
+
+    <div class="list">
+      <ul>
+        <li> <input type="checkbox"> Realizar tarefa</li>
+        <li> <input type="checkbox"> Realizar tarefa</li>
+        <li> <input type="checkbox"> Realizar tarefa</li>
+        <li> <input type="checkbox"> Realizar tarefa</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
