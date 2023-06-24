5e3a4b6a4ecc79400cca25ed363e9bd960b52d0c
Pedro Rocha <pedrorocha73658@gmail.com>
2023-06-24 12:20:21 -0300

    Styling task input text.

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="to-do">
-    <h1>Lista de Tarefas</h1>
+    <h1 class="title">Lista de Tarefas</h1>
 
     <div class="input-field">
       <label>Tarefas: </label>
-      <div>
-        <input type="text">
-        <button>Adicionar</button>
+      <div class="input-todo">
+        <input class="input-text" placeholder="Digite sua tarefa..." type="text">
+        <button class="add-button">Adicionar</button>
       </div>
     </div>
 
